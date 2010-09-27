ab1eab6b174779d1ceb4a8738c90449ced09a78d
Postmodern <postmodern.mod3@gmail.com>
2010-09-26 19:06:47 -0700

    Added Data#byte_order and Data#byte_order=.

--- lib/ffi/exif/data.rb
+++ lib/ffi/exif/data.rb
@@ -9,6 +9,34 @@ module FFI
              :size, :uint,
              :priv, :pointer
 
+      #
+      # The byte order of the EXIF data.
+      #
+      # @return [Symbol]
+      #   The byte order (`:intel` or `:motorola`).
+      #
+      def byte_order
+        EXIF.exif_data_get_byte_order(self)
+      end
+
+      #
+      # Sets the byte order of the EXIF data.
+      #
+      # @param [Symbol] new_order
+      #   The new byte order for the EXIF data.
+      #
+      # @return [Symbol]
+      #   The byte order of the EXIF data.
+      #
+      # @example
+      #   data.byte_order = :intel
+      #   data.byte_order = :motorol
+      #
+      def byte_order=(new_order)
+        EXIF.exif_data_set_byte_order(self,new_order)
+        return new_order
+      end
+
     end
   end
 end
